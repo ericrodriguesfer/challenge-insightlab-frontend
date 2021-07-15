650febdf24d37622d862e171142c420ddb2d5387
ericrodriguesfer <er601432@gmail.com>
2021-07-14 21:33:56 -0300

    add security

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chalenge-insightlab-api.herokuapp.com",
+  baseURL: process.env.REACT_APP_API_URL_CONNECTION,
 });
 
 export default api;
